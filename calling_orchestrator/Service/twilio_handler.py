e62402aa16074867c8d2d4ac660315bde1d1cf73
chris <chrismskim@naver.com>
2025-05-26 23:27:29 +0900

    [feat] twilio_hander.py 기초 세팅 완료

--- calling_orchestrator/Service/twilio_handler.py
+++ calling_orchestrator/Service/twilio_handler.py
@@ -1,1 +1,16 @@
 #통화 연결
+from twilio.rest import Client
+
+# Twilio 계정 SID와 인증 토큰 설정
+account_sid = '계정 SID 값'  # 실제 값으로 변경
+auth_token = 'token'    # 실제 값으로 변경
+client = Client(account_sid, auth_token)
+
+# 전화 걸기
+call = client.calls.create(
+    to='+82~~~',  # 수신자 전화번호 (국가번호 포함)
+    from_='+1~',      # 구매한 Twilio 전화번호
+    url='http://demo.twilio.com/docs/voice.xml'  # 통화 시 실행될 TwiML URL
+)
+
+print(call.sid)  # 통화 ID 출력
